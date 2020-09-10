1944f10eaf4d86c5367314a0c8a59565372f0ea3
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-09-10 09:19:29 +0100

    Update process-hostfile.sh
    Fixed issue where the system doesn't work if the first host in the file is not the headnode and were if only one host speficied it didn't work.

--- distrac/process-hostfile.sh
+++ distrac/process-hostfile.sh
@@ -18,11 +18,10 @@ case $i in
     ;;
 esac
 done
-# Read hostfile and put the amount of hosts in amountOfHosts.num
-echo `awk -F '[,]' ' {print NF}' $hostfile` > $folder/amountOfHosts.num
 # Convert the hostfile to a column of hosts and store in $folder/hostlist
 tr , '\n' <  $hostfile >  $folder/hostfile
 # Make sure the first host in hostfile is the headnode
 sed -i "/\b\("$HOSTNAME"\)\b/d" $folder/hostfile
-sed -i "1 i$HOSTNAME" $folder/hostfile
-
+echo  "$(printf "$HOSTNAME \n"; cat $folder/hostfile)" > $folder/hostfile
+# Read hostfile and put the amount of hosts in amountOfHosts.num
+cat $folder/hostfile | wc -l > $folder/amountOfHosts.num

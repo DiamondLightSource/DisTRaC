441db14053bcd81ea17a1cd460dbb1f462b7c782
gmw99 <gabryelmw@gmail.com>
2021-08-02 17:52:45 +0000

    Added BRD Ram block option
    This loads the brd block device and creates the relevant pv's

---
+++ distrac/create-brd.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+size=""
+amount=0
+folder="."
+for i in "$@"
+do
+case $i in
+    -s=*|--size=*)
+    size="${i#*=}"
+    shift # past argument=value
+    ;;
+    -n=*|--number=*)
+    amount="${i#*=}"
+    shift # past argument=value
+    ;;
+    -f=*|--folder=*)
+    folder="${i#*=}"
+    mkdir $folder
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+# Load brd ram block module
+sudo modprobe brd rd_size=`echo $(( $(numfmt $size --from iec) / 1024 )) ` max_part=1 rd_nr=$amount
+sudo pvcreate /dev/ram[0-$((amount-1))] &
+wait

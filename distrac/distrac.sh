97c88ca6834689311c0c5db830fcbf343ec500ba
gmw99 <gabryelmw@gmail.com>
2021-08-02 18:20:09 +0000

    Updated distrac
    Fixed issue where rgw would run even when not flagged to do so

--- distrac/distrac.sh
+++ distrac/distrac.sh
@@ -24,23 +24,23 @@ case $i in
     -t=*|--type=*)
     type="${i#*=}"
     shift
-	;;
+    ;;
     -s=*|--size=*)
     size="${i#*=}"
     shift # past argument=value
     ;;
     -n=*|--number=*)
     amount="${i#*=}"
-	shift # past argument=value
+    shift # past argument=value
     ;;
-	 -pn=*|--poolname=*)
+    -pn=*|--poolname=*)
     poolname="${i#*=}"
     shift # past argument=value
     ;;
-	-rgw|--rgw)
-	rgw=true
-	shift
-	;;
+    -rgw|--rgw)
+    rgw=true
+    shift
+    ;;
     -uid=*|--uid=*)
     id="${i#*=}"
     shift # past argument=value
@@ -49,18 +49,18 @@ case $i in
     secret="${i#*=}"
     shift # past argument=value
     ;;
-	 -hf=*|--hostfile=*)
+    -hf=*|--hostfile=*)
     hostfile="${i#*=}"
     shift # past argument=value
     ;;
-	-h| --help)
-	helpmsg
-	exit 0 
-	shift
-	;;
+    -h| --help)
+    helpmsg
+    exit 0 
+    shift
+    ;;
     *) 
-	helpmsg
-	exit 0
+    helpmsg
+    exit 0
     ;;
 esac
 done
@@ -86,7 +86,7 @@ fi
 
 module unload openmpi
 
-if ([ $rgw ] && [ -z "$poolname" ])
+if ([ "$rgw" = "true" ] && [ -z "$poolname" ])
 	 then
 	./create-rgw.sh -f=$folder
 	if ([ -z "$id" ] && [ -z "$secret" ])

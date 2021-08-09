0ee259ad43e7d0aac2010f3cbbd707a1dcd481f8
gmw99 <gabryelmw@gmail.com>
2021-08-09 09:54:35 +0000

    Updated examples
    The example now include the -t flag so that they work with the new update.

--- examples/example-run-multi-osds.sh
+++ examples/example-run-multi-osds.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Start"
 cd ../distrac/
-time ./distrac.sh -i=ib0 -s=10G -n=6 -pn=example 
+time ./distrac.sh -i=ib0 -s=10G -n=6 -t=gram -pn=example 
 # USER JOB
 ceph -s 
 # END OF USER JOB
-time ./remove-distrac.sh
+time ./remove-distrac.sh -t=gram
 cd ../examples
 echo "Done"
--- examples/example-run-rados.sh
+++ examples/example-run-rados.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Start"
 cd ../distrac/
-time ./distrac.sh -i=ib0 -s=90G -n=1 -pn=example 
+time ./distrac.sh -i=ib0 -s=90G -n=1 -t=gram -pn=example 
 # USER JOB
 cd ../examples
 rados bench -p example  20  write  --no-cleanup > radosWrite.log
 rados bench -p example 20 rand > radosRand.log
 # END OF USER JOB
 cd ../distrac/
-time ./remove-distrac.sh
+time ./remove-distrac.sh -t=gram
 cd ../examples
 echo "Done"

ce5d217e56d40d7bab6101cb77f1c3fbb8feef5e
= <=>
2022-06-26 17:36:57 +0100

    Remove pvremove
    This is not required as the pv are removed when the module is unloaded.

--- distrac/remove-zram.sh
+++ distrac/remove-zram.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 # Remove ZRAM block device
 
-# Get amount of zram's 
-amount=` find /dev/ -name "zram*" | wc -l`
-# Remove PV's
-sudo pvremove /dev/zram[0-$((amount-1))] &
-wait
 # Unload zram
 sudo rmmod zram  
 remove-zram-lvm.sh
